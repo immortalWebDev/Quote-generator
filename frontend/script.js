ae370703a138cb82f3f0c87d4e08b58239a97904
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-29 13:57:44 +0530

    Added loading state

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,11 @@
+const quoteText = document.getElementById("quoteText");
 let quotes = [];
 async function fetchQuotes() {
   try {
     // const response = await fetch("http://localhost:3000/api/quotes");
-    const response = await fetch("https://quote-generator-backend-hrnf.onrender.com/api/quotes");
-
+    const response = await fetch(
+      "https://quote-generator-backend-hrnf.onrender.com/api/quotes"
+    );
 
     const data = await response.json();
 
@@ -15,6 +17,8 @@ async function fetchQuotes() {
     quotes = data;
     // console.log("Quotes loaded:", quotes);
   } catch (error) {
+    quoteText.textContent = "Error loading quotes.";
+
     console.error("Failed to fetch quotes data:", error);
   }
 }
@@ -22,10 +26,13 @@ async function fetchQuotes() {
 //Display random one
 function generateQuote() {
   if (quotes.length === 0) {
-    console.log("Lading quotes...");
+    console.log("Loading quotes...");
+    quoteText.textContent = "Fetching fresh quote...";
     return;
   }
   const randomIndex = Math.floor(Math.random() * quotes.length);
+  quoteText.textContent = quotes[randomIndex].text || "No quote found";
+
   const quote = quotes[randomIndex];
   document.getElementById("quoteText").textContent = `"${quote.text}"`;
   document.getElementById("authorText").textContent = `- ${quote.author}`;
